d8304d63eb447ed72f7935a936274dea3678b6c6
imacaveman <153271834+imacaveman@users.noreply.github.com>
2024-01-18 15:43:32 +0000

    everything done I believe

--- js/myjs.js
+++ js/myjs.js
@@ -1,7 +1,15 @@
+// *************** VARIABLES *************** //
+
 const $headerTitle = $("#title");
 const $headerBio = $("#bio");
 const $hamburger = $(".hamburger");
+const $hamburgerContainer = $(".burger-container");
+const $contactName = $("#contact-name");
+const $contactSurname = $("#contact-surname");
+const $contactEmail = $("#contact-email");
 const $sidebar = $(".sidebar");
+
+// ************ END OF VARIABLES ************ //
 
 $headerBio.hide().delay(1500).fadeIn(2000);
 
@@ -13,11 +21,39 @@ setTimeout(function() {
     $headerTitle.addClass("title-glow-off");
 }, 3000);
 
-$(".burger-container").on("click", function(event){
+$hamburgerContainer.on("click", function(event){
     $hamburger.toggleClass("is-active");
-    $sidebar.
+    $(this).toggleClass("burger-clicked");
+    $sidebar.toggleClass("show");
 });
 
-$("#contact-name").attr('required',true);
-$("#contact-surname").attr('required',true);
-$("#contact-email").attr('required',true);
+$(".sidebar-nav a").on("click", function(event){
+    $hamburger.toggleClass("is-active");
+    $hamburgerContainer.toggleClass("burger-clicked");
+    $sidebar.toggleClass("show");
+});
+
+$($contactName).attr('required',true);
+$($contactSurname).attr('required',true);
+$($contactEmail).attr('required',true);
+
+$(document).ready(function() {
+    $(".contact-submit").click(function() {
+        var emailInput = $("#contact-email").val();
+        if (isValidEmail(emailInput)) {
+            console.log("Valid email address!");
+        } else {
+            $("#contact-email").addClass("warning-input");
+            $("#contact-email").val("Invalid email address");
+            console.log("Invalid email address.");
+        }
+    });
+    function isValidEmail(email) {
+        var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        return emailRegex.test(email);
+    }
+});
+
+$("#contact-email").on("click", function(event){
+    $(this).removeClass("warning-input");
+});
